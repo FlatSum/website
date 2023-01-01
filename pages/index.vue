c212526c4b4c65c59bbe375b437c830e223d1aee
kabatafw <kabata@four-wood.co.jp>
2023-01-01 23:37:39 +0900

    second commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fv-container">
+    <video class="fv" src="~assets/img/fv.mp4" autoplay muted loop ></video>
+  </div>
+</template>
+<style lang="scss" scoped>
+.fv-container {
+  width: 100vw;
+  height: auto;
+
+  .fv {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
